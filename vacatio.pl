48727331d69ac769cb4f1528940ce4866c8523e8
SzymonMajk <Szymek_diks@interia.pl>
2021-11-15 10:38:11 +0100

    Changes after first consultations

--- vacatio.pl
+++ vacatio.pl
@@ -1,121 +1,30 @@
 :- dynamic
-    xpozytywne/1,
     xodrzucona/1.
 
 % Baza ofert
-% ofertaX - zlozona zawsze z atomow dla predykatow poziomu nizej np. oferta1 -> gory, niska itd. 
-% ofertaX - czesciowa to kombinacja predykatow oferty
+% oferta(Id, Od, Do, Lokalizacja, Inne) - Inne jako lista, i zostają do określenia TODO
+% np.
+% oferta (oferta1, '2021-11-12', '2021-12-12', lokalizacja(magagaskar), cena(wartosc), [ tutaj inne cechy jeszcze ])
 
-oferta_jest(oferta1) :- not(odrzucona(oferta1)),
-                        lokalizacja_docelowa(gory),
-                        cena(niska),
-                        kuchnia(syta),
-                        atrakcja(spa),
-                        atrakcja(wycieczka),
-                        zakwaterowanie(hotel).
+oferta(oferta1, '2022-06-12', '2022-06-18', lokalizacja(rodos), cena(3000), [wyspa, morze, cieplo, relaks]).
 
-oferta_czesciowo_jest(oferta1) :- not(odrzucona(oferta1)),
-                                  lokalizacja_docelowa(gory),
-                                  cena(niska).
+% Predykaty zapytań
+% Wybierane i wykorzystywane przez zewnętrzne API, po zadaniu odpowiednich pytań, po przecinku z oferta
 
-oferta_czesciowo_jest(oferta1) :- not(odrzucona(oferta1)),
-                                  kuchnia(syta),
-                                  atrakcja(spa).
+klimat(lokalizacja(rodos), srodziemnomorski).
+klimat(lokalizacja(ateny), srodziemnomorski).
 
-oferta_czesciowo_jest(oferta1) :- not(odrzucona(oferta1)),
-                                  atrakcja(wycieczka),
-                                  zakwaterowanie(hotel).
-
-% Predykaty niższego stopnia
-
-lokalizacja_docelowa(gory) :- pozytywne(wysilek_fizyczny),
-                              pozytywne(piekne_widoki),
-                              pozytywne(las),
-                              pozytywne(swieze_powietrze).
-
-cena(niska) :- pozytywne(camping),
-               pozytywne(bary_mleczne),
-               pozytywne(brak_pamiatek).
-
-cena(niska) :- pozytywne(tani_hotel),
-               pozytywne(bary_mleczne),
-               pozytywne(brak_pamiatek).
-
-kuchnia(syta) :- pozytywne(tlusta),
-                 pozytywne(karczma),
-                 pozytywne(obfita).
-
-kuchnia(miejska) :- pozytywne(zbilansowana),
-                    pozytywne(bary_mleczne).
-
-atrakcja(spa) :- pozytywne(relaks),
-                 pozytywne(odnowa_biologiczna),
-                 pozytywne(basen),
-                 pozytywne(masaz).
-
-atrakcja(wycieczka) :- pozytywne(wysilek_fizyczny),
-                       pozytywne(zorganizowane),
-                       pozytywne(gromadnie).
-
-atrakcja(wycieczka) :- pozytywne(wysilek_fizyczny),
-                       pozytywne(niezorganizowane),
-                       pozytywne(samotnie).
-
-zakwaterowanie(hotel) :- pozytywne(male_bagaze),
-                         pozytywne(mala_swoboda),
-                         pozytywne(bezpieczenstwo).
-
-pozytywne(X) :- xpozytywne(X).
+cenowo(cena(C), tania) :- C < 1500.
+cenowo(cena(C), budzetowa) :- C < 3500.
 
 odrzucona(X) :- xodrzucona(X).
 
 % API
 
-pamietaj(X) :- not(pozytywne(X)), assertz(xpozytywne(X)).
-
 odrzuc(X) :- not(odrzucona(X)), assertz(xodrzucona(X)).
 
-zapomnij :- retractall(xpozytywne(_)),
-            retractall(xodrzucona(_)).
+zapomnij :- retractall(xodrzucona(_)).
 
 % Testy
 
-pamietaj_sym :- pamietaj(camping),
-                pamietaj(bary_mleczne),
-                pamietaj(brak_pamiatek),
-                pamietaj(wysilek_fizyczny),
-                pamietaj(piekne_widoki),
-                pamietaj(las),
-                pamietaj(swieze_powietrze),
-                pamietaj(tlusta),
-                pamietaj(karczma),
-                pamietaj(obfita),
-                pamietaj(relaks),
-                pamietaj(odnowa_biologiczna),
-                pamietaj(basen),
-                pamietaj(masaz),
-                pamietaj(zorganizowane),
-                pamietaj(gromadnie),
-                pamietaj(male_bagaze),
-                pamietaj(mala_swoboda),
-                pamietaj(bezpieczenstwo).
-
-pamietaj_czesciowo_sym :- pamietaj(camping),
-                pamietaj(bary_mleczne),
-                pamietaj(brak_pamiatek),
-                pamietaj(wysilek_fizyczny),
-                pamietaj(las),
-                pamietaj(tlusta),
-                pamietaj(karczma),
-                pamietaj(obfita),
-                pamietaj(relaks),
-                pamietaj(odnowa_biologiczna),
-                pamietaj(basen),
-                pamietaj(masaz),
-                pamietaj(zorganizowane),
-                pamietaj(gromadnie),
-                pamietaj(male_bagaze),
-                pamietaj(mala_swoboda),
-                pamietaj(bezpieczenstwo).
-
-% oferta_jest(X) oraz oferta_czesciowo_jest(X)
+% oferta(Id, Od, Do, lokalizacja(X), cena(C), T), klimat(lokalizacja(X), srodziemnomorski), klimat(lokalizacja(X), srodziemnomorski), cenowo(cena(C), budzetowa).

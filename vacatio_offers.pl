75d9c89c71c26d584887e91b085245c4d4ab67d0
SzymonMajk <Szymek_diks@interia.pl>
2021-12-04 18:18:59 +0100

    One offer dates change

--- vacatio_offers.pl
+++ vacatio_offers.pl
@@ -6,4 +6,4 @@ oferta(oferta2, '2022-06-09', '2022-06-18', lokalizacja(ateny), cena(3000), [kol
 oferta(oferta3, '2022-06-05', '2022-06-08', lokalizacja(zakopane), cena(7000), [trekking, szlaki, galerie, koscioly]).
 oferta(oferta4, '2022-06-14', '2022-06-19', lokalizacja(krakow), cena(4000), [galerie, koscioly, golebie, muzea]).
 oferta(oferta5, '2022-06-13', '2022-06-30', lokalizacja(widacz), cena(500), [zwierzeta, agroturystyka, swieze_powietrze]).
-oferta(oferta6, '2022-06-12', '2022-06-21', lokalizacja(kreta), cena(3000), [wyspa, morze, cieplo, relaks]).
+oferta(oferta6, '2022-06-14', '2022-06-25', lokalizacja(kreta), cena(3000), [wyspa, morze, cieplo, relaks]).

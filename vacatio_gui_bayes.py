f2e593f08820351eeed1344d296362db8b43bd25
Jakub Caputa <jjakubcaputa@gmail.com>
2022-01-24 08:45:44 +0100

    final version

--- vacatio_gui_bayes.py
+++ vacatio_gui_bayes.py
@@ -51,15 +51,17 @@ class App(Frame):
         self.climate_list = ['srodziemnomorski', 'gorski', 'umiarkowany']
         self.prices = ['budzetowa', 'tania', 'droga']
         self.location_types = ['weekendowa', 'tygodniowa', 'dwutygodniowa']
+        self.threshold_list=[0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
         self.canceled_offers = []
         self.offered = False
         self.to_not_accept_current = None
 
         self.quit_btn = Button(root, text="Wyjdź", command=self.quit).pack(pady=5)
 
-
-
-
+        self.selected_threshold = StringVar()
+        self.selected_threshold.set(self.threshold_list[0])
+        self.threshold_drop = OptionMenu(root, self.selected_threshold, *self.threshold_list)
+        self.threshold_drop.pack(pady=5)
 
         self.selected_climate = StringVar()
         self.selected_climate.set(self.climate_list[0])
@@ -79,16 +81,16 @@ class App(Frame):
 
         self.location_type_drop.pack(pady=5)
 
-        var1 = IntVar()
-        var2 = IntVar()
-        var3 = IntVar()
-        var4 = IntVar()
-        var5 = IntVar()
-        c1 = Checkbutton(root, text='Morze', variable=var1, onvalue=1, offvalue=0)
-        c2 = Checkbutton(root, text='Wyspa', variable=var2, onvalue=1, offvalue=0)
-        c3 = Checkbutton(root, text='Szlaki', variable=var3, onvalue=1, offvalue=0)
-        c4 = Checkbutton(root, text='Swiatynie', variable=var4, onvalue=1, offvalue=0)
-        c5 = Checkbutton(root, text='Swieze Powietrze', variable=var5, onvalue=1, offvalue=0)
+        self.var1 = IntVar()
+        self.var2 = IntVar()
+        self.var3 = IntVar()
+        self.var4 = IntVar()
+        self.var5 = IntVar()
+        c1 = Checkbutton(root, text='Morze', variable=self.var1, onvalue=1, offvalue=0)
+        c2 = Checkbutton(root, text='Wyspa', variable=self.var2, onvalue=1, offvalue=0)
+        c3 = Checkbutton(root, text='Szlaki', variable=self.var3, onvalue=1, offvalue=0)
+        c4 = Checkbutton(root, text='Swiatynie', variable=self.var4, onvalue=1, offvalue=0)
+        c5 = Checkbutton(root, text='Swieze Powietrze', variable=self.var5, onvalue=1, offvalue=0)
         c1.pack()
         c2.pack()
         c3.pack()
@@ -96,7 +98,7 @@ class App(Frame):
         c5.pack()
         self.search_offer = Button(root, text="Dopasuj oferte", command=self.find_offer)
         self.search_offer.pack(pady=5)
-        self.offer_text = Label(root, text="Offer: ")
+        self.offer_text = Label(root, text="Offer: ",height=11, width=35)
         self.offer_text.pack(pady=5)
         self.accept_offer = Button(root, text="Wybierz", command=self.accept)
         self.decline_offer = Button(root, text="Odrzuć", command=self.decline)
@@ -137,7 +139,23 @@ class App(Frame):
 
     def find_offer(self):
         if not self.offered:
+            self.threshold = float(self.selected_threshold.get())
             self.change_evidence_and_update(self.net, "Pytanie_o_klimat_2", self.selected_climate.get())
+            self.change_evidence_and_update(self.net, "Pytanie_o_cene", self.selected_price.get())
+            self.change_evidence_and_update(self.net, "Pytaine_o_dlugosc_wycieczki", self.selected_location_type.get())
+            if self.var1.get()==1:
+                self.change_evidence_and_update(self.net, "morze", "Tak")
+            if self.var2.get()==1:
+                self.change_evidence_and_update(self.net, "wyspa", "Tak")
+            if self.var3.get()==1:
+                self.change_evidence_and_update(self.net, "szlaki2", "Tak")
+            if self.var4.get()==1:
+                self.change_evidence_and_update(self.net, "swiatynie", "Tak")
+            if self.var5.get()==1:
+                self.change_evidence_and_update(self.net, "swieze_powietrze", "Tak")
+            self.net.update_beliefs()
+            offer = self.return_offers(self.net)
+            self.offer_text.config(text=offer)
 
             # query = DynamicQuery()
             # query.set_from(datetime.strptime(self.cal_from.get_date(), '%m/%d/%y'))
@@ -191,6 +209,30 @@ class App(Frame):
 
                       ")=" + str(posteriors[i]))
 
+    def return_offers(self, net):
+        d = net.get_all_nodes()
+        list_offers=[]
+        for node_handle in net.get_all_nodes():
+            if node_handle in [11, 12, 13, 14, 15]:
+                node_id = net.get_node_id(node_handle)
+
+                if net.is_evidence(node_handle):
+
+                    print(node_id + " has evidence set (" +
+
+                          net.get_outcome_id(node_handle,
+
+                                             net.get_evidence(node_handle)) + ")")
+
+                else:
+
+                    posteriors = net.get_node_value(node_handle)
+
+                    for i in range(0, len(posteriors)):
+                        #print("P(" + node_id + "=" + net.get_outcome_id(node_handle, i) + ")=" + str(posteriors[i]))
+                        if posteriors[i] >= self.threshold and net.get_outcome_id(node_handle, i) == "take":
+                            list_offers.append("P(" + node_id + "=" + net.get_outcome_id(node_handle, i) + ")=" + str(round(posteriors[i], 3))+"\n")
+        return list_offers
     def print_all_posteriors(self, net):
 
         for handle in net.get_all_nodes():
